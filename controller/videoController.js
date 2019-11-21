57871e6540ee5479d7e41008ed96fd5f6552326b
Seojin Kim <1106laura@naver.com>
2019-11-21 20:46:20 +0900

    2019-11-21

---
+++ controller/videoController.js
@@ -0,0 +1,23 @@
+import {videos} from "../db"
+
+export const home = (req,res) => 
+    res.render("Home", {pageTitle:"Home", videos});
+
+export const search = (req, res) => {
+    const { 
+        query: { term: searchingBy } 
+    } = req;
+    res.render("Search", {pageTitle:"Search", searchingBy});
+}
+
+export const upload = (req, res) => 
+    res.render("Upload", {pageTitle:"Upload"});
+
+export const videoDetail = (req, res) => 
+    res.render("VideoDetail", {pageTitle:"Video Detail"});
+
+export const editVideo = (req, res) => 
+    res.render("EditVideo", {pageTitle:"Edit Video"});
+
+export const deleteVideo = (req, res) => 
+    res.render("DeleteVideo", {pageTitle:"Delete Video"});
