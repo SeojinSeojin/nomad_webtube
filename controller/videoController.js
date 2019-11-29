c66c3102f902a2588568ffe0d907b73dc5136219
Seojin Kim <1106laura@naver.com>
2019-11-29 11:48:56 +0900

    2019-11-29

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,17 +1,40 @@
-import {videos} from "../db"
+import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req,res) => 
-    res.render("Home", {pageTitle:"Home", videos});
+export const home = async(req,res) => {
+    try{
+        const videos = await Video.find({});
+        res.render("home", {pageTitle:"Home", videos});
+    } catch(error){
+        console.log(error);
+        res.render("Home", {pageTitle:"Home", videos : []});
+    }
+    
+}
 
 export const search = (req, res) => {
     const { 
         query: { term: searchingBy } 
     } = req;
-    res.render("Search", {pageTitle:"Search", searchingBy});
+    res.render("Search", {pageTitle:"Search", searchingBy, videos});
 }
 
-export const upload = (req, res) => 
+export const getUpload = (req, res) => 
     res.render("Upload", {pageTitle:"Upload"});
+
+export const postUpload = async(req, res) => {
+    const{
+        body:{title, description},
+        file:{path}
+    }= req;
+    const newVideo = await Video.create({
+        fileUrl:path,
+        title,
+        description
+    });
+    console.log(newVideo);
+    res.redirect(routes.videoDetail(newVideo.id));
+}
 
 export const videoDetail = (req, res) => 
     res.render("VideoDetail", {pageTitle:"Video Detail"});
--- db.js
+++ db.js
@@ -1,50 +1,19 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
     {
-      id: 324393,
-      title: "Video awesome",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    },
-    {
-      id: 1212121,
-      title: "Video super",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    },
-    {
-      id: 55555,
-      title: "Video nice",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    },
-    {
-      id: 11111,
-      title: "Video perfect",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
+        useNewUrlParser: true, 
+        useFindAndModify: false
     }
-  ];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("O Connected to DB");
+const handleError = () => console.log("X Error on DB Connection:${error}");
+
+db.once("open", handleOpen);
+db.on("error", handleError);
